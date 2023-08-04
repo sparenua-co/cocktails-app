b073075e922458c630171a8193d9b10358dfcd3d
Shanker Sidambaram <s.sidambaram@sana-commerce.com>
2023-08-04 11:07:08 +0530

    remove redundant files

--- CAPP/src/redux/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import cocktailsReducer from './cocktailsReducer';
-
-const rootReducer = combineReducers({
-  cocktails: cocktailsReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
-export default rootReducer;
--- CAPP/src/redux/store.tsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
-
-const store = configureStore({
-    reducer: rootReducer,
-  });
-
-  export type AppDispatch = typeof store.dispatch;
-  export default store;
